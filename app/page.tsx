79a885cfdaf121f4672e1bbe014037a7a186a149
Tan12thetha <90913334+Tan12thetha@users.noreply.github.com>
2024-04-20 12:58:01 +0530

    made a basic ui of chatgpt and integrate with supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,233 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { createClient } from "@/utils/supabase/client";
+import { FiUpload } from "react-icons/fi";
+import { FiEdit } from "react-icons/fi";
+import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const supabase = createClient();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://accounts.google.com/gsi/client";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  async function handleSignInWithGoogle(response: any) {
+    const { data, error } = await supabase.auth.signInWithIdToken({
+      provider: "google",
+      token: response.credential,
+      nonce: "NONCE", // must be the same one as provided in data-nonce (if any)
+    });
+    console.log(data);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className=" flex h-screen">
+        <ScrollArea
+          className="min-w-80 max-w-80  rounded-md border p-4 bg-[#F9F9F9] 
+        "
+        >
+          {/* sidebar nav bar */}
+          <div className="bg-[#F9F9F9] py-3 px-4 flex justify-between items-center sticky top-0 ">
+            <div className="flex items-center gap-2">
+              <Image className="rounded-full border-1 text-black" src={"/logo.svg"} alt="Logo" width={30} height={30} />
+              <div className="text-black font-semibold">New Chat</div>
+            </div>
+            <div>
+              <FiEdit className="text-xl" />
+            </div>
+          </div>
+
+          {/* History */}
+          <div className="pt-10">
+            <div>
+              Jokester began sneaking into the castle in the middle of the night
+              and leaving jokes all over the place: under the king's pillow, in
+              his soup, even in the royal toilet. The king was furious, but he
+              couldn't seem to stop Jokester. And then, one day, the people of
+              the kingdom discovered that the jokes left by J okester were so
+              funny that they couldn't help but laugh. And once they started
+              laughing, they couldn't stop.
+            </div>
+            <div>
+              Jokester began sneaking into the castle in the middle of the night
+              and leaving jokes all over the place: under the king's pillow, in
+              his soup, even in the royal toilet. The king was furious, but he
+              couldn't seem to stop Jokester. And then, one day, the people of
+              the kingdom discovered that the jokes left by Jokester were so
+              funny that they couldn't help but laugh. And once they started
+              laughing, they couldn't stop.
+            </div>
+            <div>
+              Jokester began sneaking into the castle in the middle of the night
+              and leaving jokes all over the place: under the king's pillow, in
+              his soup, even in the royal toilet. The king was furious, but he
+              couldn't seem to stop Jokester. And then, one day, the people of
+              the kingdom discovered that the jokes left by Jokester were so
+              funny that they couldn't help but laugh. And once they started
+              laughing, they couldn't stop.
+            </div>
+            <div>
+              Jokester began sneaking into the castle in the middle of the night
+              and leaving jokes all over the place: under the king's pillow, in
+              his soup, even in the royal toilet. The king was furious, but he
+              couldn't seem to stop Jokester. And then, one day, the people of
+              the kingdom discovered that the jokes left by Jokester were so
+              funny that they couldn't help but laugh. And once they started
+              laughing, they couldn't stop.
+            </div>
+          </div>
+
+
+          <div className="flex sticky bottom-0 bg-[#F9F9F9] px-5 pb-4 pt-6 gap-3 items-center">
+            <Image src={"/user.png"} alt="user" width={40} height={40} className="flex-none" />
+            <div className="grow font-[500]">Tanay Srivastava</div>
+            
+          </div>
+        </ScrollArea>
+        <main className="w-full h-screen flex flex-col justify-center items-center">
+          <div className="w-full">
+            <div className="flex justify-between px-6 py-3 items-center">
+              <div className="text-xl">
+                <span className="font-semibold dark:text-white">ChatGPT</span>{" "}
+                <span className="text-gray-300">3.5</span>
+              </div>
+              <Button className="bg-white text-black border-2 shadow-none rounded-xl ">
+                <FiUpload className="text-xl" />
+              </Button>
+            </div>
+          </div>
+
+          <div className="h-full flex flex-col justify-center items-center overflow-y-auto">
+            <ScrollArea className="grow w-full  ">
+              <div className="flex justify-center">
+                <div className="w-[68%]  ">
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                  fkanfkankjankanfafnksanfksnkfnkjnnfanmn
+                </div>
+              </div>
+            </ScrollArea>
+
+            <Input
+              className="w-[68%] flex mb-10 py-8 rounded-2xl text-lg pl-8 outline-none ring-0 shadow-md"
+              type="email"
+              placeholder="Message ChatGPT.."
             />
-          </a>
-        </div>
+          </div>
+        </main>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
+}
+
+{
+  /* <div
+        id="g_id_onload"
+        data-client_id="114615926093-lau02iplo5dcvgemjjpp5nbbqpn1c7fg.apps.googleusercontent.com"
+        data-context="signup"
+        data-ux_mode="popup"
+        data-callback={handleSignInWithGoogle}
+        data-auto_prompt="false"
+      ></div>
+
+      <div
+        className="g_id_signin"
+        data-type="standard"
+        data-shape="rectangular"
+        data-theme="outline"
+        data-text="signin_with"
+        data-size="large"
+        data-logo_alignment="left"
+      ></div> */
 }
---
+++ app/signup/page.tsx
@@ -0,0 +1,118 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { signup } from "./action";
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Please provide valid email",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 2 characters.",
+  }),
+});
+
+const Signup = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const formData = new FormData();
+    formData.append("email", values.email);
+    formData.append("password", values.password);
+
+    signup(formData); // Call the signup function with the form data
+  }
+  return (
+    <div className="flex flex-col items-center justify-center pt-[5%]  tracking-wide">
+      <Image
+        src={"/logo.svg"}
+        alt="logo"
+        height={75}
+        width={75}
+        className="filter grayscale invert"
+      />
+      <div className="text-black font-bold text-4xl pt-24">Welcome Back</div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-[340px] pt-10"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                {/* <FormLabel>email</FormLabel> */}
+                <FormControl>
+                  <Input
+                    className="py-6 border-2"
+                    placeholder="Email Address"
+                    {...field}
+                  />
+                </FormControl>
+                {/* <FormDescription>
+                  This is your public display name.
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                {/* <FormLabel>Username</FormLabel> */}
+                <FormControl>
+                  <Input
+                    className="py-6 border-2"
+                    placeholder="Passoword"
+                    {...field}
+                  />
+                </FormControl>
+                {/* <FormDescription>
+                  This is your public display name.
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full bg-[#10a37f] py-7 text-lg">
+            Submit
+          </Button>
+        </form>
+      </Form>
+      <div className="text-sm pt-1">
+        <span>Have an account?</span>{" "}
+        <Link className="text-[#10a37f]" href={"/login"}>
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
