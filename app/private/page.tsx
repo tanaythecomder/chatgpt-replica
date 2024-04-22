0c57d050d88f73f44d1b20d27977e4acaa40120c
Tan12thetha <90913334+Tan12thetha@users.noreply.github.com>
2024-04-22 22:27:07 +0530

    removed anomalies

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -7,8 +7,6 @@ import func from '@/utils/gemini/gemini'
 export default async function PrivatePage() {
   const supabase = createClient()
 
-  func()
-
   const { data, error } = await supabase.auth.getUser()
   if (error || !data?.user) {
     redirect('/login')
